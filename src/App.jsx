1e55b6b7f408fb303107ad9eb13253074cdf7d4a
Lexpiper <lexpiperfx@gmail.com>
2023-04-08 08:51:09 +0100

    handled crash

--- src/App.jsx
+++ src/App.jsx
@@ -6,63 +6,48 @@ import Note from "./components/Note";
 
 function App() {
   const [notes, setNotes] = useState([]);
-  const [saved, setsaved] = useState(false);
 
   useEffect(() => {
     (async () => {
       try {
-        let td = await localStorage.getItem("noteList");
-        setNotes(JSON.parse(td));
-        // console.log(td, "///")
+        const storedNotes = await localStorage.getItem("noteList");
+        const parsedNotes = JSON.parse(storedNotes) || [];
+        setNotes(parsedNotes);
       } catch (err) {
         console.log(err.message);
       }
     })();
-    return () => {};
-  }, [saved]);
+  }, []);
 
   const addNote = (newNote) => {
-    // fetch all todolist from localstorage
-    let todolist = JSON.parse(localStorage.getItem("noteList"));
-    if (!todolist) {
-      let arr = [];
-      arr.push(newNote);
-      localStorage.setItem("noteList", JSON.stringify(arr));
-    } else if (todolist.length > 0) {
-      let newTodoList = [...todolist, newNote];
-      localStorage.setItem("noteList", JSON.stringify(newTodoList));
-    }
-    return setsaved(!saved);
+    const existingNotes = JSON.parse(localStorage.getItem("noteList")) || [];
+    const updatedNotes = [...existingNotes, newNote];
+    localStorage.setItem("noteList", JSON.stringify(updatedNotes));
+    setNotes(updatedNotes);
   };
+
   const deleteNote = (id) => {
-    setNotes((prevNote) => {
-      let updatedNotes = prevNote.filter((noteItem, index) => {
-        return index !== id;
-      });
-      localStorage.setItem("noteList", JSON.stringify(updatedNotes));
-      return updatedNotes;
-    });
+    const filteredNotes = notes.filter((_, index) => index !== id);
+    localStorage.setItem("noteList", JSON.stringify(filteredNotes));
+    setNotes(filteredNotes);
   };
+
   return (
     <div className="App dark:bg-black bg-transparent h-full min-h-screen font-poppins pb-20 overflow-x-hidden">
       <Header />
       <Input onAdd={addNote} />
       <div className="grid grid-cols-1 md:grid-cols-4 place-items-center mt-10 gap-5 h-full">
-        {notes.map((noteItem, index) => {
-          return (
-            <div className="" key={index}>
-              <Note
-                key={index}
-                id={index}
-                title={noteItem.title}
-                content={noteItem.content}
-                onDelete={deleteNote}
-              />
-            </div>
-          );
-        })}
+        {notes.map((noteItem, index) => (
+          <div className="" key={index}>
+            <Note
+              id={index}
+              title={noteItem.title}
+              content={noteItem.content}
+              onDelete={deleteNote}
+            />
+          </div>
+        ))}
       </div>
-
       <Footer />
     </div>
   );
